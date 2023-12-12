591beb88e9d53bdafb2fb1549cb3c077ad23e7f1
Aiden Ziegelaar <Aiden-Ziegelaar@users.noreply.github.com>
2023-12-12 12:45:20 +0800

    Added RdsSmCluster

--- Infrastructure/constructs/RDSClusterSMCredentials/index.ts
+++ Infrastructure/constructs/RDSClusterSMCredentials/index.ts
@@ -1,16 +1,17 @@
-import { 
-    RdsClusterInstance, 
-    RdsClusterInstanceConfig 
-} from "@cdktf/provider-aws/lib/rds-cluster-instance";
+import { RdsCluster, RdsClusterConfig } from "@cdktf/provider-aws/lib/rds-cluster";
 import { Construct } from "constructs";
 
-interface RdsSmClusterInstanceConfig extends RdsClusterInstanceConfig {
+interface RdsSmClusterConfig extends RdsClusterConfig {
     manage_master_user_password: true;
     password: never;
 }
 
-export class RdsSmClusterInstance extends RdsClusterInstance {
-    constructor(scope: Construct, name: string, config: RdsSmClusterInstanceConfig) {
+export class RdsSmClusterInstance extends RdsCluster {
+    constructor(
+        scope: Construct, 
+        name: string, 
+        config: RdsSmClusterConfig
+    ) {
         super(scope, name, config);
     }
 }
